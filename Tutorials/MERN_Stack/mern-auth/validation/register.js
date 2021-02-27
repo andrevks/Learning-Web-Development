c50358d1bb0c07c1a856fb20476d7d3c0491874b
andrevks <italiabk@gmail.com>
2021-02-27 00:50:26 -0400

    chore: Register endpoint

--- Tutorials/MERN_Stack/mern-auth/validation/register.js
+++ Tutorials/MERN_Stack/mern-auth/validation/register.js
@@ -15,7 +15,6 @@ module.exports = function ValidateRegisterInput(data){
   if(Validator.isEmpty(data.name)){
     errors.name = "Name field is required";
   }
-
   //Email checks
   if(Validator.isEmpty(data.email)){
     errors.email = "Email field is required";
