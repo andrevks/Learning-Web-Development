d91b16672d9a6dcef8ab1993ca3fe65287b80ac0
andrevks <italiabk@gmail.com>
2021-02-28 00:37:12 -0400

    chore: login endpoint

--- Tutorials/MERN_Stack/mern-auth/routes/api/users.js
+++ Tutorials/MERN_Stack/mern-auth/routes/api/users.js
@@ -46,3 +46,62 @@ router.post("/register", (req, res)=>{
   });
 
 });
+
+// @route POST api/users/login
+// @desc Login user and return JWT token
+// @access Public
+
+router.post("/login", (req, res) => {
+
+  //form validation
+
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  //Checks validation
+  if(!isValid){
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //Find user by email
+  User.findOne({ email }).then(user => {
+    //check if user exists
+    if(!user){
+      return res.status(400).json({ emailnotfound: "Email not found"});
+    }
+    //Check password
+    bcrypt.compare(password, user.password).then(isMatch =>{
+      if(isMatch){
+        //User matched
+        //Create JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.name
+        };
+
+        //sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 31556926 // 1 year in seconds
+          },
+          (err, token)=>{
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      }else{
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect"});
+      }
+    });
+  });
+});
+
+module.exports = router;
