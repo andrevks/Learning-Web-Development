0989dbc777da2674729d47ec0177d34fc078f996
andrevks <italiabk@gmail.com>
2021-02-28 00:21:36 -0400

    chore: passport setup

--- Tutorials/MERN_Stack/mern-auth/config/keys.js
+++ Tutorials/MERN_Stack/mern-auth/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://mernUser:mern123@cluster0.riaqc.mongodb.net/mernTest?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://mernUser:mern123@cluster0.riaqc.mongodb.net/mernTest?retryWrites=true&w=majority",
+    secretOrKey: "secret"
 };
