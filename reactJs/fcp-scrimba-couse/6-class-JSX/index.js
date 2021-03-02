28f9a4efb9adea69ba5e5e620117fcd92cd96b27
andrevks <italiabk@gmail.com>
2021-03-02 01:26:13 -0400

    build: reactDom class 6

---
+++ reactJs/fcp-scrimba-couse/6-class-JSX/index.js
@@ -0,0 +1,21 @@
+// Objective: Fill in the boilerplate React code required to render an
+// unordered list (<ul>) to the page. The list should contain 3 list items
+// (<li>) with anything in them you want.
+
+// HINTS:
+// import the libraries you need first
+// use one of the libraries to render some JSX to the page
+
+import React from "react"
+import ReactDom from "react-dom"
+
+ReactDom.render(
+  
+  <div>
+    <ul>
+      <li>Overnight</li>
+      <li>Success</li>
+      <li>Is a Mith</li>
+    </ul>
+  </div>
+, document.getElementById("root"));
---
+++ reactJs/fcp-scrimba-couse/README.md
@@ -0,0 +1,5 @@
+# Learn React with Bob Ziroll on Scrimba
+
+
+[Resourse](https://scrimba.com/learn/learnreact)
+
