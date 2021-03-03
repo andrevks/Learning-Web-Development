d090804e49513ece7309412b2422c08cabe88de2
andrevks <italiabk@gmail.com>
2021-03-02 23:48:02 -0400

    feat: navbar created

---
+++ Tutorials/MERN_Stack/mern-auth/client/src/components/layout/Navbar.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+
+class Navbar extends Component {
+  render(){
+    return(
+
+      <div className="navbar-fixed">
+        <nav className="z-depth-0">
+          <div className="nav-wrapper white">
+            <Link
+              to="/"
+              sytle={
+                {
+                  fontFamily: "monospace"
+                }
+              }
+              className="col s5 brand-logo center black-text"
+            />
+            <i className="material-icons">code</i>
+            MERN
+          </div>
+        </nav>
+      </div>
+
+    );
+  }
+}
+
+export default Navbar;
