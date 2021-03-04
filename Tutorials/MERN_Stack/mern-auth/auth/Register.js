e15a3f74c841ef0608cf13109669334f57a470cc
andrevks <italiabk@gmail.com>
2021-03-04 00:47:13 -0400

    build: onChange and onSubmit events

--- Tutorials/MERN_Stack/mern-auth/auth/Register.js
+++ Tutorials/MERN_Stack/mern-auth/auth/Register.js
@@ -20,5 +20,17 @@ class Register extends Component{
 
   onSubmit = e => {
     e.preventDefault();
-  }
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    }
+
+    console.log(newUser);
+  
+  };
+
+  
 }
