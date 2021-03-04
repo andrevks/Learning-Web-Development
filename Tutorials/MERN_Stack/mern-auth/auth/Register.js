459bdb7d69ddc6b3dd4731eecadfb3e5915bf37a
andrevks <italiabk@gmail.com>
2021-03-04 00:43:06 -0400

    build: register component state

---
+++ Tutorials/MERN_Stack/mern-auth/auth/Register.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+
+class Register extends Component{
+  constructor(){
+    super();
+    this.state ={
+      name: "",
+      email:"",
+      password:"",
+      password2:"",
+      errors: {}
+    };
+  }
+
+  onChange = e =>{
+    this.setState({ [e.target.id ]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+  }
+}
