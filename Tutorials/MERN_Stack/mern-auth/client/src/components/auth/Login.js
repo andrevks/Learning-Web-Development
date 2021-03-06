e7627cd0b2db467bcdd571899ea70bc862852c5a
andrevks <italiabk@gmail.com>
2021-03-06 01:46:13 -0400

    chore: block user acess to login in case the user is logged in

--- Tutorials/MERN_Stack/mern-auth/client/src/components/auth/Login.js
+++ Tutorials/MERN_Stack/mern-auth/client/src/components/auth/Login.js
@@ -14,6 +14,12 @@ class Login extends Component {
       errors: {}
     };
   }
+  componentDidMount() {
+    // If logged in and user navigates to Login page, should redirect them to dashboard
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
 
   componentWillReceiveProps(nextProps){
     if(nextProps.auth.isAuthenticated){
