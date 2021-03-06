a83134e5a2e14a1e78860eeaa9787e96737ad7c8
andrevks <italiabk@gmail.com>
2021-03-06 01:42:11 -0400

    build: check localStorage and expired token

--- Tutorials/MERN_Stack/mern-auth/client/src/App.js
+++ Tutorials/MERN_Stack/mern-auth/client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -9,6 +12,29 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
+
+//Checks for token to keep user logged in 
+if(localStorage.jwtToken){
+  //set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  //decode token and get user infor and exp
+  const decoded = jwt_decode(token);
+  //set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  //checks for expired token
+  const currentTime = Date.now() / 1000; //to get in milliseconds
+  if(decoded.exp < currentTime){
+    //logout user
+    store.dispatch(logoutUser());
+
+    //redirect to login
+    window.location.href = "./login";
+  }
+}
 
 class App extends Component {
   render() {
@@ -20,6 +46,9 @@ class App extends Component {
             <Route exact path="/" component={Landing}/>
             <Route exact path="/register" component={Register}/>
             <Route exact path="/login" component={Login}/>
+            <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+            </Switch>
           </div>
         </Router>
       </Provider>
