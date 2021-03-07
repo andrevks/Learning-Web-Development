d7e990bd717ea438551486cc908527b0e39eac69
andrevks <italiabk@gmail.com>
2021-03-06 23:10:37 -0400

    build:MyTaste component

---
+++ reactJs/fcp-scrimba-couse/10-class-organization/components/MyTaste.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MyTaste(){
+    return (
+        <div>
+            <ol>
+                <li>Meat</li>
+                <li>Rice</li>
+                <li>Beans</li>
+            </ol>
+        </div>
+    );
+}
+
+export default MyTaste;
