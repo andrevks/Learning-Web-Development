71fe6002c270fcb8c9558f43d7a988d84ae338de
andrevks <italiabk@gmail.com>
2021-03-11 00:47:30 -0400

    build: render app component in separate file

---
+++ reactJs/fcp-scrimba-couse/12 - class - parent  practice/MainContent.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function MainContent(){
+  return(
+    <p>
+      Main content right here :D
+    
+    </p>
+  );
+}
+
+export default MainContent;
---
+++ reactJs/fcp-scrimba-couse/12 - class - parent  practice/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar(){
+  return(
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>News</li>
+        <li>Contact</li>
+        <li>About</li>
+      </ul>
+    </div>
+  ); 
+  
+}
+
+export default Navbar;
