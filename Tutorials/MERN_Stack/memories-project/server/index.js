594103110db07bd97250c7efabe973ce9f5a5f56
andrevks <italiabk@gmail.com>
2021-03-12 01:09:09 -0400

    build: setting up server and it's not working

--- Tutorials/MERN_Stack/memories-project/server/index.js
+++ Tutorials/MERN_Stack/memories-project/server/index.js
@@ -3,5 +3,25 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended:true}))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended:true}))
+app.use(cors());
+
+// mongodb.com/clould/atlas
+
+const CONNECTION_URL = "mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.riaqc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+//Connect to db
+mongoose.connect(CONNECTION_URL, { useNewUrlParser:true, useUnifiedTopology:true })
+.then(()=>//it returns a promise 
+  app.listen(PORT, () => console.log(`Server runningon port: ${PORT}`))) 
+.catch((error) => console.log(error.message));
+
+
+mongoose.set("useFindAndModify", false)
+
 
 
