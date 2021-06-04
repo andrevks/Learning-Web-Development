b8b8815ebb0e331ba503fc85cc87353c67b68178
André <39144691+andrevks@users.noreply.github.com>
2021-06-04 13:59:44 -0400

    update: description

--- CodingChallenges/twoSum.py
+++ CodingChallenges/twoSum.py
@@ -1,3 +1,13 @@
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Runtime: 3872 ms, faster than 5.75% of Python online submissions for Two Sum.
+Memory Usage: 14.3 MB, less than 23.18% of Python online submissions for Two Sum.
+'''
 def twoSum(nums, target):
     for i in range(0, len(nums)):
         for j in range(i + 1, len(nums)):
