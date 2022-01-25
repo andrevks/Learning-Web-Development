e04c4f7ae2497cd6b837c36d077e690530ba168b
andrevks <italiabk@gmail.com>
2022-01-24 21:58:15 -0400

    build: class 1 -> const, let and comentaries

---
+++ JavaScript/roger-melo/aula 1/app.js
@@ -0,0 +1,19 @@
+
+let age = 31
+let currentYear = 2019
+
+console.log(age, currentYear)
+
+age = 36 
+
+console.log(age)
+
+const points = 100 
+ 
+console.log(points)
+
+// camelCase
+
+// aksdflasdf
+
+
